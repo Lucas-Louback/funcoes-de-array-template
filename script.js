16a0a7afba26a1ff91d51dffd9f5619bc3313fd6
Lucas-Louback <lucasmlouback99@gmail.com>
2023-04-03 11:22:18 -0300

    template pronto

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Com o array de pokémons disponibilizado, faça o que se pede:
+// Declare uma função que receba como parâmetro o objeto poke, e que defina a propriedade vida como 100.
+// Utilize o map() e a função criada no item 1 para alterar a propriedade vida de todos os objetos do array pokemons. Guarde o novo array numa variável pokemonsVidaCheia.
+// Utilize o filter() e uma função não-nomeada para retornar apenas os pokémons de fogo. Guarde o novo array numa variável pokemonsDeFogo.
+
+
 const pokemons = [
   { nome: 'Bulbasaur', tipo: 'grama', vida: 40 },
   { nome: 'Bellsprout', tipo: 'grama', vida: 20 },
@@ -6,3 +12,55 @@ const pokemons = [
   { nome: 'Squirtle', tipo: 'água', vida: 45 },
   { nome: 'Psyduck', tipo: 'água', vida: 25 }
 ]
+
+function alteraVida(poke) {
+  return {...poke, vida: 100}
+}
+alteraVida(pokemons)
+
+const pokemonsVidaCheia = pokemons.map(alteraVida)
+console.log (pokemonsVidaCheia)
+
+const pokemonDeFogo = pokemons.filter((fogo) => {
+  return fogo.tipo === "fogo"
+})
+console.log(pokemonDeFogo)
+console.log(pokemons)
+
+
+// const multiplicaPor3 = (array) => {
+//   const novoArray = []
+//   for (let i = 0; i < array.length; i++) {
+//     const numeroMultiplicado = array[i] * 3
+//     novoArray.push(numeroMultiplicado)
+//   }
+//   return novoArray
+// }
+// const arrNumeros = [0, 8, 55, 6, 3, 7]
+
+// const retornaPares = (array) => {
+//   const novoArray = []
+//   for (let elemento of array) {
+//     if (elemento % 2 === 0) {
+//       novoArray.push(elemento)
+//     }
+//   }
+//   return novoArray
+// }
+// console.log("arrNumeros", arrNumeros)
+// console.log("multiplica por 3", multiplicaPor3(arrNumeros))
+// console.log("retorna Pares", retornaPares(arrNumeros))
+
+
+// const arrayMultiplicadoPor3 = arrNumeros.map((elemento, indice, arrayInteiro) => {
+//   return elemento * 3
+// })
+
+// console.log(arrayMultiplicadoPor3)
+
+// const arrayPares = arrNumeros.filter((elemento) => {
+//   if (elemento % 2 === 0) {
+//     return true
+//   }
+// })
+// console.log(arrayPares)
